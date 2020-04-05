7b6751ffe3a4ad5b71ead06c5093c3e1a0928b2d
Anjali Gopinathan <asgopina@usc.edu>
2020-04-04 17:24:20 -0700

    odometry shtuff

--- src/robonaldo_hardware/src/odometry.cpp
+++ src/robonaldo_hardware/src/odometry.cpp
@@ -13,7 +13,7 @@ const float inches_per_tick = 0.0245f; // 8*3.14 / 1024 = 0.0245 (circumference 
 float X = 0.0;
 float Y = 0.0;
 float THETA = 0.0;
-float VELOCITY = 0.0;
+float FORWARD_VEL = 0.0;
 float ANGULAR_VEL = 0.0;
 float last_left_count = 0.0;
 float last_right_count = 0.0;
@@ -26,7 +26,9 @@ void odomCallback(const robonaldo::encoder_values::ConstPtr& msg) {
   float delta_dist = (delta_left_count + delta_right_count) / 2.0f;
   float delta_theta = (delta_right_count - delta_left_count)/ROBOT_WIDTH;
 
-  VELOCITY = (msg->left_velocity + msg->right_velocity)*(inches_per_tick) / 2.0f;
+  FORWARD_VEL = (msg->left_velocity + msg->right_velocity)*(inches_per_tick) / 2.0f;
+  //CCW is positive angle
+  ANGULAR_VEL = (msg->left_velocity - msg->right_velocity)*(inches_per_tick)/ROBOT_WIDTH;
 
   X += ( delta_dist * cos(delta_theta) );
   Y += ( delta_dist * sin(delta_theta) );
@@ -43,7 +45,6 @@ int main(int argc, char **argv) {
   ros::Subscriber encoder_sub = n.subscribe("encoder_values", 1000, odomCallback);
   ros::Publisher odom_pub = n.advertise<robonaldo::odometry>("odometry", 1000);
   ros::Rate loop_rate(10);
-  int count = 0;
 
 	while (ros::ok()) {
 		// This is a message object. stuff it with data and publish it
@@ -51,6 +52,8 @@ int main(int argc, char **argv) {
 		msg.x = X;
 		msg.y = Y;
     msg.theta = THETA;
+    msg.forward_velocity = FORWARD_VEL;
+    msg.angular_velocity = ANGULAR_VEL;
 
 		ROS_INFO("ODOMETRY_PUBLISHER: Sending x=%f, y=%f, theta=%f",msg.x, msg.y, msg.theta);
 
@@ -59,7 +62,6 @@ int main(int argc, char **argv) {
 		ros::spinOnce();
 
 		loop_rate.sleep();
-		++count;
 	}
 
 	return 0;
