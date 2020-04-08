00583db9cab29a28d444622c4cb557c18f42155b
Alex Colello <alexlcolello@gmail.com>
2020-04-08 16:05:41 -0700

    Added ROS to gazebo plugin
    Co-authored-by: Ethan Yu <Ethan-yu0503@gmail.com>

--- gazebo/robonaldo_plugin.cpp
+++ gazebo/robonaldo_plugin.cpp
@@ -6,6 +6,12 @@
 #include <gazebo/transport/transport.hh>
 #include <gazebo/msgs/msgs.hh>
 
+#include <thread>
+#include "ros/ros.h"
+#include "ros/callback_queue.h"
+#include "ros/subscribe_options.h"
+#include "std_msgs/Float32.h"
+
 namespace gazebo
 {
   /// \brief A plugin to control the Robonaldo robot.
@@ -44,7 +50,7 @@ namespace gazebo
       // Add another joint
 
       // Setup a P-controller, with a gain of 0.1.
-      this->pid = common::PID(0.1, 0, 0);
+      this->pid = common::PID(0.5, 0, 0);
 
       // Apply the P-controller to the joint.
       this->model->GetJointController()->SetVelocityPID(
@@ -59,22 +65,32 @@ namespace gazebo
           this->joint0->GetScopedName(), 0.0);
       this->model->GetJointController()->SetVelocityTarget(
           this->joint1->GetScopedName(), 0.0);
-      
-      // Create the node
-      this->node = transport::NodePtr(new transport::Node());
-      #if GAZEBO_MAJOR_VERSION < 8
-      this->node->Init(this->model->GetWorld()->GetName());
-      #else
-      this->node->Init(this->model->GetWorld()->Name());
-      #endif
 
-      // Create a topic name
-      std::string topicName = "~/" + this->model->GetName() + "/vel_cmd";
-      std::cout << "topic Name is " << topicName << std::endl;
+      // Initialize ros, if it has not already bee initialized.
+      if (!ros::isInitialized())
+      {
+        int argc = 0;
+        char **argv = NULL;
+        ros::init(argc, argv, "gazebo_client",
+            ros::init_options::NoSigintHandler);
+      }
 
-      // Subscribe to the topic, and register a callback
-      this->sub = this->node->Subscribe(topicName,
-         &RobonaldoPlugin::OnMsg, this);
+      // Create our ROS node. This acts in a similar manner to
+      // the Gazebo node
+      this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
+
+      // Create a named topic, and subscribe to it.
+      ros::SubscribeOptions so =
+        ros::SubscribeOptions::create<std_msgs::Float32>(
+            "/" + this->model->GetName() + "/vel_cmd",
+            1,
+            boost::bind(&RobonaldoPlugin::OnRosMsg, this, _1),
+            ros::VoidPtr(), &this->rosQueue);
+      this->rosSub = this->rosNode->subscribe(so);
+
+      // Spin up the queue helper thread.
+      this->rosQueueThread =
+        std::thread(std::bind(&RobonaldoPlugin::QueueThread, this));
     }
 
     // \brief Set the velocity of the Velodyne
@@ -86,13 +102,22 @@ namespace gazebo
           this->joint0->GetScopedName(), _vel);
     }
 
-    /// \brief Handle incoming message
-    /// \param[in] _msg Repurpose a vector3 message. This function will
-    /// only use the x component.
-    private: void OnMsg(ConstVector3dPtr &_msg)
+    /// \brief Handle an incoming message from ROS
+    /// \param[in] _msg A float value that is used to set the velocity
+    /// of the Velodyne.
+    public: void OnRosMsg(const std_msgs::Float32ConstPtr &_msg)
     {
-      std::cout << "OnMsg" << std::endl;
-      this->SetVelocity(_msg->x());
+      this->SetVelocity(_msg->data);
+    }
+
+    /// \brief ROS helper function that processes messages
+    private: void QueueThread()
+    {
+      static const double timeout = 0.01;
+      while (this->rosNode->ok())
+      {
+        this->rosQueue.callAvailable(ros::WallDuration(timeout));
+      }
     }
 
     /// \brief Pointer to the model.
@@ -105,11 +130,17 @@ namespace gazebo
     /// \brief A PID controller for the joint.
     private: common::PID pid;
 
-    /// \brief A node used for transport
-    private: transport::NodePtr node;
+    /// \brief A node use for ROS transport
+    private: std::unique_ptr<ros::NodeHandle> rosNode;
 
-    /// \brief A subscriber to a named topic.
-    private: transport::SubscriberPtr sub;
+    /// \brief A ROS subscriber
+    private: ros::Subscriber rosSub;
+
+    /// \brief A ROS callbackqueue that helps process messages
+    private: ros::CallbackQueue rosQueue;
+
+    /// \brief A thread the keeps running the rosQueue
+    private: std::thread rosQueueThread;
   };
 
   // Tell Gazebo about this plugin, so that Gazebo can call Load on this plugin.
