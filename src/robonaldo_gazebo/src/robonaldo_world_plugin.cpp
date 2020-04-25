7a160b4e1003aa1faeb651a9c5884bc37f6bfca9
Alex Colello <alexlcolello@gmail.com>
2020-04-25 15:10:01 -0700

    Fixed some issues with the gazebo world plugin

--- src/robonaldo_gazebo/src/robonaldo_world_plugin.cpp
+++ src/robonaldo_gazebo/src/robonaldo_world_plugin.cpp
@@ -1,15 +1,19 @@
 #include <ignition/math/Pose3.hh>
-#include "gazebo/physics/physics.hh"
+#include <gazebo/physics/physics.hh>
 #include "gazebo/common/common.hh"
 #include "gazebo/gazebo.hh"
+#include "ros/ros.h"
 
 namespace gazebo {
 
 class RobonaldoWorldPlugin : public WorldPlugin {
 	public: void Load(physics::WorldPtr _parent, sdf::ElementPtr /*_sdf*/) {
+
+		std::cerr << "\nThe robonaldo world plugin is correctly attached\n";
+
 		if (_parent->ModelCount() == 0) {
 		    std::cerr << "Invalid model count, Robonaldo World plugin not loaded\n";
-        return;
+        	return;
 		}
 
 		this->world = _parent;
@@ -22,20 +26,25 @@ class RobonaldoWorldPlugin : public WorldPlugin {
 		// Initialize ros, if it has not already bee initialized.
 		if (!ros::isInitialized())
 		{
-		int argc = 0;
-		char **argv = NULL;
-		ros::init(argc, argv, "gazebo_client",  // TODO: change this node name
-		    ros::init_options::NoSigintHandler);
+			int argc = 0;
+			char **argv = NULL;
+			ros::init(argc, argv, "robonaldo_gazebo_world",  // TODO: change this node name
+			    ros::init_options::NoSigintHandler);
 		}
+
+        // Create our ROS node. This acts in a similar manner to
+        // the Gazebo node
+        this->rosNode.reset(new ros::NodeHandle("robonaldo_gazebo_world"));
 
 		// Create our ROS node. This acts in a similar manner to the Gazebo node.
 		
 
 
 	}
-	private: physcs::WorldPtr world;
+	private: physics::WorldPtr world;
 	private: physics::ModelPtr robo;
 	private: physics::ModelPtr ball;
+    private: std::unique_ptr<ros::NodeHandle> rosNode;
 
 };
 
